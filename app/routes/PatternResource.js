301ad48a7e974674fdae96eb5ce1d6e75791882e
Nour ElDin Khaled <nkhaled16@outlook.com>
2017-04-06 12:01:03 +0200

    amir

--- app/routes/PatternResource.js
+++ app/routes/PatternResource.js
@@ -2,7 +2,7 @@ module.exports = function(app){
 	var Pattern = require('../controllers/PatternController');
 	var auth = require('../middlewares/AuthMiddleware');
 
-	app.get('/api/pattern',auth,Pattern.getPatterns);
-	app.put('/api/pattern/:sequenceid/:eventid',auth,Pattern.update);
+	app.get('/api/patterns',auth,Pattern.getPatterns);
+	app.put('/api/patterns/:sequenceid/:eventid',auth,Pattern.update);
 
 };
